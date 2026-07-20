8b79991443ddf1613ed8f5076a63a3cab7924b60
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 17:43:36 +0600

    recap the past module

--- recap.js
+++ recap.js
@@ -1,3 +1,18 @@
 for(let i=1; i<50; i++){
     console.log(i);
 }
+
+/**
+ * variable
+ */
+
+const price = 50;
+const studentName = 'Gobbar';
+const isRich = true;
+
+if(studentName==='talukdar'){
+    console.log();
+}
+else{
+    console.log();
+}
